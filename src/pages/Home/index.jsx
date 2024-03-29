93f3417f4f2764db06119cc6cb2038e83b0306d2
Benny Chang <c.a.s.winterleaf@gmail.com>
2024-03-29 17:22:23 +0800

    feat: add remove icon feature

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,14 +2,21 @@ import Carousel from './Carousel';
 import Products from './Products';
 import home from '../../css/Home.module.css'
 import saleIcon from './flash-sale-icon.png'
+import { useState } from 'react';
 
 function Home() {
+  const [removeIcon, setRemoveIcon] = useState(false)
+
+  const handleRemoveIcon = () => {
+    setRemoveIcon(true)
+  }
+
   return (
     <>
       <Carousel />
-      <div className={home.icon}>
+      <div style={{ display: removeIcon ? 'none' : 'block' }} className={home.icon}>
         <div className={home.iconLayout}>
-          <div className={home.removeIcon}>
+          <div onClick={handleRemoveIcon} className={home.removeIcon}>
             <p>x</p>
           </div>
           <a href="./flashsale"><img src={saleIcon} alt="icon" /></a>

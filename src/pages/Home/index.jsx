2fa38152abe27bf2264c14a3f59adc59a8b3ee68
Benny Chang <c.a.s.winterleaf@gmail.com>
2024-03-29 17:08:06 +0800

    feat(style): homepage flashsale icon UI

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,21 @@
 import Carousel from './Carousel';
 import Products from './Products';
+import home from '../../css/Home.module.css'
+import saleIcon from './flash-sale-icon.png'
 
 function Home() {
   return (
     <>
       <Carousel />
+      <div className={home.icon}>
+        <div className={home.iconLayout}>
+          <div className={home.removeIcon}>
+            <p>x</p>
+          </div>
+          <a href="./flashsale"><img src={saleIcon} alt="icon" /></a>
+          <p className={home.timer}>00:00:00</p>        
+        </div>
+      </div>
       <Products />
     </>
   );

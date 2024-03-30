073eeb5877ef933ea36ff548c15fd07357de09ac
yingying <18636abc@gmai.com>
2024-03-30 23:58:40 +0800

    setting: try modify styled component setting

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-
+import macrosPlugin from "vite-plugin-babel-macros";
 export default defineConfig(() => {
   return {
-    plugins: [react()],
+    plugins: [react(), macrosPlugin()],
     css: {
       modules: true, // set CSS Modules
     },

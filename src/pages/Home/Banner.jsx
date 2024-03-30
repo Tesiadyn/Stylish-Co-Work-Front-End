628c41bbb0fd83e08b98210e2665bffb8a431afc
yingying <18636abc@gmai.com>
2024-03-30 11:33:45 +0800

    improve: implement banner logic with api

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,10 +1,22 @@
 import React, { useEffect } from "react";
 import styles from "./Banner.module.css";
 import { useState } from "react";
-
+import newApi from "../../utils/newApi";
+import api from "../../utils/api";
 const Banner = () => {
-  const [modalClassName, setModalClassName] = useState(styles.modal);
-
+  const { modal, closedModal } = styles;
+  const [modalClassName, setModalClassName] = useState(closedModal);
+  useEffect(() => {
+    // (async function getUserData(){
+    //     const data=await newApi.getUserData()
+    //     // if chance >0 ==> setModalClassName(modal)
+    // })()
+    ////////////////test
+    // (async function getCampaigns() {
+    //   const { data } = await api.getCampaigns();
+    //   if (data.length > 0) setModalClassName(modal);
+    // })();
+  }, []);
   return (
     <>
       {/* The Modal */}
@@ -14,7 +26,7 @@ const Banner = () => {
           <div className={styles.modalHeader}>
             <span
               className={styles.close}
-              onClick={() => setModalClassName(styles.closedModal)}
+              onClick={() => setModalClassName(closedModal)}
             >
               ×
             </span>
